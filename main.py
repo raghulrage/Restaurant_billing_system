30256cc19a93aca626b0ddbf57fcad6c01ce08aa
raghulrage <46341793+raghulrage@users.noreply.github.com>
2020-05-13 17:10:11 +0530

    Uploaded files

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pytz import timezone
+
 food_list = []
 ordered_list = []
 
@@ -12,12 +15,12 @@ class Order(Food):
                 self.total = quantity*price
 
                 Food.__init__(self,name,price)
-        
+
 
 def main():
         while True:
                 print('\n'*5)
-                print(' MAIN MENU '.center(60,'*')+'\n\n\t(O)Order Food\n\t(C)View Cart\n\t(E)Exit\n'+'_'*60+'\n')
+                print(' MAIN MENU '.center(60,'*')+'\n\n\t(O)Order Food\n\t(C)View Cart\n\t(R)Report\n\t(E)Exit\n'+'_'*60+'\n')
                 option = input('Select an Option: ').upper()
 
                 if option == 'O':
@@ -26,11 +29,12 @@ def main():
                         view_cart()
                 if option == 'E':
                         exit_menu()
-                        break
+                if option == 'R':
+                        view_report()
                 else:
                         print('Invalid option, Enter Again!!!')
 
-        
+
 def load_food():
         food_items = open('files/food_list.txt','r')
         for food in sorted(food_items):
@@ -38,40 +42,43 @@ def load_food():
                 if food:
                         food_list.append(Food(*food.rsplit(' ',1)))
 
+def view_report():
+        report = open('files/report.txt','r').read()
+        print(report)
 def view_cart():
         while 1:
                 print('\n'*5)
                 print(' CART '.center(60,'*')+'\n')
                 total = 0
                 if ordered_list:
-                        
+
                         print('\t|NO|'.ljust(5,' ')+'|FOOD NAME|'.ljust(15,' ')+'|PRICE x QUANTITY = TOTAL|'+'\n')
                         for i,food in enumerate(ordered_list):
-                                total +=food.total 
+                                total +=food.total
                                 print('\t{:<5}{:<15}{:<5}x   {:<5} =  Rs.{:<4}'.format(i+1,food.name,food.price,food.quantity,food.total))
                         print('\t'+'-'*46)
                         print('\tTotal    '+('Rs. '+str(total)).rjust(35,' '))
                         print('\n(M)Main Menu'+' '*3+'(R)Remove Item'+' '*3+'(O)Order Food'+'\n'+'(P)Payment'+' '*5+'(E)Exit')
                         print('_'*60+'\n')
-                        option = input()
-                        if option.upper() == 'M':
+                        option = input().upper()
+                        if option == 'M':
                                 main()
-                        elif option.upper() == 'P':
+                        elif option == 'P':
                                 payment(total)
-                        elif option.upper() == 'E':
+                        elif option == 'E':
                                 exit_menu()
-                        elif option.upper() == 'O':
+                        elif option == 'O':
                                 order_food()
-                        elif option.upper() == 'R':
+                        elif option == 'R':
                                 remove_item()
                         else:
                                 print('Invalid option, Enter Again!!!')
-                        
-                        
+
+
                 else:
                         print('\n'+'Cart is empty'.center(60,'-'))
                         main()
-        
+
 def remove_item():
         option = int(input('\nEnter Number to Remove: '))
         if option >  len(ordered_list):
@@ -79,16 +86,17 @@ def remove_item():
         else:
                 item = ordered_list.pop(option-1)
                 print(item.name+' Removed...')
-                
+
 def payment(total):
         if ordered_list:
                 print('\nTotal amount: Rs.',total)
                 print('\n'+'Payment Successful!!!\n')
+                report(total)
         else:
                 print('Cart is empty')
         main()
-                
-                
+
+
 def order_food():
         while True:
                 print('\n'*5)
@@ -114,8 +122,7 @@ def order_food():
                                         ordered_list.append(Order(food.name,food.price,quantity))
                         else:
                                 print('Enter valid data!!!')
-                                
-                                        
+
                 elif food_id.upper() == 'E':
                         exit_menu()
                 elif food_id.upper() == 'C':
@@ -124,9 +131,34 @@ def order_food():
                         main()
                 else:
                         print('Invalid option, Enter Again!!!')
+
+def report(total):
+
+    file = open('files/report.txt','a')
+    report_data = '\n' + '\t\t' + '*'*40 + '\n'
+
+    now = datetime.now()
+    date = '-'.join(list(map(str,[now.day,now.month,now.year])))
+
+    now_utc = datetime.now(timezone('UTC'))
+    time = now_utc.astimezone(timezone('Asia/Kolkata'))
+    time = time.strftime("%I:%M:%S %p" )
+
+    report_data += '\t\t' + date.center(40,' ') + '\n' + '\t\t' + time.center(40,' ') + '\n' + '\t\t' + '-'*40 + '\n'
+    
+    for i,item in enumerate(ordered_list):
+        temp = '\t\t  ' + str(i+1) + ' '*5 +  item.name.center(20,' ') + 'Rs. '+str(item.price) + '\n'
+        report_data += temp
+        
+    report_data += '\t\t' + '_'*40 + '\n' + '\t\t' + '  Total' + ' '*20 + str(total) + '\n\n'
+    report_data += '\t\t' + '*'*40 + '\n\n'
+
+    file.write(report_data)
+
 def exit_menu():
         print('\n'+'THANK YOU'.center(50,'*'))
         quit()
+
 if __name__ == '__main__':
         load_food()
         main()
